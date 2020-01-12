e8ac612a823a2e354550345ce44dc7c0fe0e789c
Christie Partington <christiedpartington@gmail.com>
2020-01-11 23:16:53 -0700

    Initial commit

---
+++ scripts/get_images.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://www.serebii.net/swordshield/pokemon/831.png -O wooloo.png -P ../images
+wget https://www.serebii.net/swordshield/pokemon/835.png -O yamper.png -P ../images
+wget https://www.serebii.net/swordshield/pokemon/037.png -O vulpix.png -P ../images
