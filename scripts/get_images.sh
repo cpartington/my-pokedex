6a6408445198c7db9acaf0d0d16e2e71bb915b5c
Christie Partington <christiedpartington@gmail.com>
2020-01-12 17:54:31 -0700

    Reformat image script

--- scripts/get_images.sh
+++ scripts/get_images.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-wget https://www.serebii.net/swordshield/pokemon/831.png -O wooloo.png -P ../images
-wget https://www.serebii.net/swordshield/pokemon/835.png -O yamper.png -P ../images
-wget https://www.serebii.net/swordshield/pokemon/037.png -O vulpix.png -P ../images
+files=(831.png wooloo.png 835.png yamper.png 037.png vulpix.png)
+url=https://www.serebii.net/swordshield/pokemon/
+inc=0
+
+while [ $inc -lt 6 ]; do
+  wget $url${files[$inc]} -O ${files[$inc+1]} -P ../images
+  let inc+=2
+done
