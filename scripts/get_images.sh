bc6d120661a74e25d4a0e4df107c6b6ccd7fcfec
Christie Partington <christiedpartington@gmail.com>
2020-01-27 20:58:33 -0700

    Fix file names

--- scripts/get_images.sh
+++ scripts/get_images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-files=(831.png wooloo.png 835.png yamper.png 037.png vulpix.png 077.png ponyta.png 242.png hattrem.png 349.png snom.png 231.png gastrodon.png 204.png sylveon.png 196.png eevee.png 191.png pumpkaboo.png 128.png cherubi.png 100.png wooper.png 092.png natu.png 090.png munna.png 055.png oddish.png)
+files=(831.png wooloo.png 835.png yamper.png 037.png vulpix.png 077-g.png ponyta.png 857.png hattrem.png 872.png snom.png 423-e.png gastrodon.png 700.png sylveon.png 133.png eevee.png 710.png pumpkaboo.png 420.png cherubi.png 194.png wooper.png 177.png natu.png 517.png munna.png 043.png oddish.png)
 
 url=https://www.serebii.net/swordshield/pokemon/
 inc=0
 
 while [ $inc -lt ${#files[@]} ]; do
-  wget $url${files[$inc]} -O ${files[$inc+1]} -P ../images
+  wget -q $url${files[$inc]} -O ${files[$inc+1]} -P ../images
   let inc+=2
 done
