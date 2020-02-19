9b36bfe953f5efde4d4dea33709eaff0d388317d
Christie Partington <christiedpartington@gmail.com>
2020-02-19 01:46:04 +0000

    Add more pokemon

--- scripts/get_images.sh
+++ scripts/get_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=(831.png wooloo.png 835.png yamper.png 037.png vulpix.png 077-g.png ponyta.png 857.png hattrem.png 872.png snom.png 423-e.png gastrodon.png 700.png sylveon.png 133.png eevee.png 710.png pumpkaboo.png 420.png cherubi.png 194.png wooper.png 177.png natu.png 517.png munna.png 043.png oddish.png 771.png pyukumuku.png 537.png seismitoad.png 534.png conkeldurr.png 861.png grimmsnarl.png 860.png morgrem.png 464.png rhyperior.png 772.png typenull.png 569.png garbodor.png 356.png dusclops.png 660.png diggersby.png 885.png dreepy.png)
+files=(831.png wooloo.png 835.png yamper.png 037.png vulpix.png 077-g.png ponyta.png 857.png hattrem.png 872.png snom.png 423-e.png gastrodon.png 700.png sylveon.png 133.png eevee.png 710.png pumpkaboo.png 420.png cherubi.png 194.png wooper.png 177.png natu.png 517.png munna.png 043.png oddish.png 771.png pyukumuku.png 537.png seismitoad.png 534.png conkeldurr.png 861.png grimmsnarl.png 860.png morgrem.png 464.png rhyperior.png 772.png typenull.png 569.png garbodor.png 356.png dusclops.png 660.png diggersby.png 885.png dreepy.png 246.png larvitar.png 704.png goomy.png)
 
 url=https://www.serebii.net/swordshield/pokemon/
 inc=0
